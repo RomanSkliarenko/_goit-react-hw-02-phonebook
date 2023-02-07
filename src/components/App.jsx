b0d2061aeae01c1e322bbb146c13a537e421cc34
Roman Skliarenko <vurtnevk@gmail.com>
2023-02-07 22:25:37 +0200

    "add localStorage store logic"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import FindContactByName from './findContactByName';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -60,7 +55,8 @@ class App extends Component {
   };
 
   componentDidMount() {
-    this.setState({ ...this.state, contacts: JSON.parse(localStorage.getItem('contactList')) });
+    const dataFromLocalStorage =  JSON.parse(localStorage.getItem('contactList'))
+    dataFromLocalStorage && this.setState({ ...this.state, contacts:dataFromLocalStorage });
   }
 
   render() {
--- src/components/contactListMarkup.jsx
+++ src/components/contactListMarkup.jsx
@@ -1,9 +1,9 @@
 const ContactListMarkup = ({ contacts, deleteContactHandler, filter }) => {
-  const filteredContactsArr = contacts && contacts.filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()));
+  const filteredContactsArr = contacts.filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()));
   return (
     <>
       {
-        contacts && filteredContactsArr.map(({ name, id, number }) =>
+        filteredContactsArr.map(({ name, id, number }) =>
           <li key={id}>{name}
             <b>:</b> {number}
             <button onClick={() => deleteContactHandler(id)}>Delete</button>
